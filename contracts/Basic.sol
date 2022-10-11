9718d79bb0ee870a9cd4c435523fccbdb5476c8f
Somjit Nag <somjitnag192@gmail.com>
2022-10-11 07:52:42 +0530

    failing: send value, see persisted value

--- contracts/Basic.sol
+++ contracts/Basic.sol
@@ -12,14 +12,23 @@ import "hardhat/console.sol";
  */
 contract Basic is ReentrancyGuard, Ownable {
   mapping(address => uint256) private balanceOf;
+  uint256 private s_counter;
 
-  event Received(address recipient, address from, uint256 amount, uint256 total); // print details for received amount
+  event Received(address recipient, address from, uint256 amount, uint256 total, uint256 counter); // print details for received amount
+  event Created(address creator, address owner, uint256 initialAmount, uint256 counter); // print details for received amount
+
+  constructor(uint256 counter) payable {
+    s_counter = counter;
+    balanceOf[msg.sender] = msg.value;
+    emit Created(msg.sender, owner(), msg.value, s_counter);
+  }
 
   // give funds to this contract's owner
   receive() external payable {
     balanceOf[owner()] += msg.value;
+    s_counter++;
     console.log("Received %s from address: %s, to be added to %s", msg.value, msg.sender, owner());
-    emit Received(owner(), msg.sender, msg.value, balanceOf[owner()]);
+    emit Received(owner(), msg.sender, msg.value, balanceOf[owner()], s_counter);
   }
 
   function getBalance() external view returns (uint256) {
@@ -28,5 +37,9 @@ contract Basic is ReentrancyGuard, Ownable {
 
   function getBalanceOf(address addr) external view returns (uint256) {
     return balanceOf[addr];
+  }
+
+  function getCounter() public view returns (uint256) {
+    return s_counter;
   }
 }
--- deploy/05-deploy-coinish.ts
+++ deploy/05-deploy-coinish.ts
@@ -20,8 +20,9 @@ module.exports = async (hre: HardhatRuntimeEnvironment) => {
   const provider = new ethers.providers.JsonRpcProvider(localHHNodeUrl);
   const wallet = new ethers.Wallet(key_deployingAccount, provider);
 
-  const deployTx = Basic.getDeployTransaction();
+  const deployTx = Basic.getDeployTransaction([1]);
   const tx = await wallet.populateTransaction(deployTx); // nonce added, among other things
+  // tx.value = ethers.utils.parseEther("1"); // WHY data too big ??!!
 
   console.log("populatedTransaction: ", tx);
   console.log("Wallet: " + wallet.address);
@@ -34,7 +35,9 @@ module.exports = async (hre: HardhatRuntimeEnvironment) => {
   const response = await provider.sendTransaction(signedTx);
   const receipt = await response.wait();
   const address = receipt.contractAddress;
-  console.log("Contract Basic deployed to address: ", address);
+  const basic = (await ethers.getContractAt("Basic", address, wallet)) as Basic;
+  console.log("Receipt : ", address, " , contract: ", basic.address);
+  console.log("Basic's starting counter: ", await basic.getCounter());
 
   // ------------- ^ the deploy part works, but below balance keeps returning 0 value ---------
 
@@ -43,12 +46,14 @@ module.exports = async (hre: HardhatRuntimeEnvironment) => {
     value: ethers.utils.parseEther("0.1"),
   });
 
-  const basic = (await ethers.getContractAt("Basic", address, wallet)) as Basic;
   // ^^ giving wallet in the 3rd argument is needed, else it will consider some default signer,
   // which will cause error because the contract was not deployed using a default signer
 
-  console.log("Basic's owner: ", await basic.owner());
-  console.log("Basic's owner balance: ", (await basic.getBalanceOf(wallet.address)).toString());
+  const ownerAddress = await basic.owner();
+  console.log("Basic's owner: ", ownerAddress);
+
+  // WHY coming as 0 always
+  console.log("Basic's owner balance: ", (await basic.getBalanceOf(ownerAddress)).toString());
 };
 
 module.exports.tags = ["all", "COIN"];
